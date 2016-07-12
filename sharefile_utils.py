7bdfc91da15bc36d85bf798f1e5328cdf633563a
R <r4dat@users.noreply.github.com>
2016-07-12 16:53:16 -0500

    Update sharefile_utils.py
    Add Apache 2.0 License, reference to original python 2 version and MIT license wrt Citrix.

--- sharefile_utils.py
+++ sharefile_utils.py
@@ -3,6 +3,26 @@
 Created on Tue Jun 16 10:41:31 2015
 
 @author: r4
+
+Copyright (c) 2015, Rob R, KHC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Original Python 2 version 
+Copyright (c) 2014 Citrix Systems, Inc.
+MIT License
+Available July 07, 2016
+https://api.sharefile.com/rest/samples/python.aspx
 """
 __author__ = 'r4'
 
